73197161688cb5343cc99d4cbd18463bc405bd99
Vnadh <106485321+Vnadh@users.noreply.github.com>
2024-07-06 20:49:09 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import { SearchBar } from "./";
-import Image from "./Image";
 //import Image from 'mui-image';
 
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <Image/>
+    <img src="https://cdn-icons-png.flaticon.com/512/3665/3665989.png" style={{height:80, width:60}}/>
     </Link>
     <SearchBar />
   </Stack>
