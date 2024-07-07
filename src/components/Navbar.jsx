0ff8a9b2ebedc8584010bdddd7afbe6de44d3c7b
Vnadh <106485321+Vnadh@users.noreply.github.com>
2024-07-07 20:17:02 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { SearchBar } from "./";
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-    <img src="https://cdn-icons-png.flaticon.com/512/3665/3665989.png" style={{height:80, width:60}}/>
+    <h2 >Flux</h2>
     </Link>
     <SearchBar />
   </Stack>
